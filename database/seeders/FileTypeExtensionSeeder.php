c323b2990a4e89fb55baad67a55f9943430edeb6
cuongnguyen9309 <cuongnguyen9309@gmail.com>
2023-06-23 21:34:36 +0700

    .

--- database/seeders/FileTypeExtensionSeeder.php
+++ database/seeders/FileTypeExtensionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\FileType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,9 +14,17 @@ class FileTypeExtensionSeeder extends Seeder
      */
     public function run(): void
     {
+        $imageFileType = FileType::where('name', 'image')->first();
+        $audioFileType = FileType::where('name', 'audio')->first();
+        $videoFileType = FileType::where('name', 'video')->first();
+        $textFileType = FileType::where('name', 'text')->first();
         $items = [
-            ['file_type_id' => 1, 'name' => 'png'],
-            ['file_type_id' => 10, 'name' => 'txt'],
+            ['file_type_id' => $imageFileType->id, 'name' => 'png'],
+            ['file_type_id' => $imageFileType->id, 'name' => 'jpg'],
+            ['file_type_id' => $imageFileType->id, 'name' => 'jpeg'],
+            ['file_type_id' => $audioFileType->id, 'name' => 'mp3'],
+            ['file_type_id' => $videoFileType->id, 'name' => 'mp4'],
+            ['file_type_id' => $textFileType->id, 'name' => 'txt'],
         ];
         DB::table('file_type_extension')->insert($items);
     }
